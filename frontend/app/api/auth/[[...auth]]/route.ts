3f4049be068ad1485c551a47a1016f5f624cfa6f
abdul rehman <160239442+abdulrehman1101@users.noreply.github.com>
2026-01-17 13:58:10 +0500

    phase 1 to 5

--- frontend/app/api/auth/[[...auth]]/route.ts
+++ frontend/app/api/auth/[[...auth]]/route.ts
@@ -1,5 +1,5 @@
 import { auth } from "@/src/lib/auth"; // Adjust the import path as needed
-import { handleAuth } from "better-auth/frameworks/next-js";
+import { handleAuth } from "better-auth/next-js";
 
 // This API route handles all Better Auth requests
 export const { GET, POST } = handleAuth(auth);
